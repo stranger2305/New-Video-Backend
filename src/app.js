34e4be668683c80fed937df585d32a2c3b4be5de
YashPowar23 <yashpowar1095@gmail.com>
2026-03-01 02:00:23 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static('public')) //to serve static files
 app.use(cookieParser()) //to parse cookies
 
+//routes
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
 export { app }
